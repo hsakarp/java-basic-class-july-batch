92e957d3001bf14637ef556cb7f2d7a428ca3883
bhismakandel <bhisma.kandel@gmail.com>
2019-08-05 19:11:56 -0400

    For loop assignment modified

--- Bhisma_Assignment_Files/forLoopPattern.java
+++ Bhisma_Assignment_Files/forLoopPattern.java
@@ -22,25 +22,16 @@ public class forLoopPattern {
 	*/
 		
 // Pattern #3
-		/*
-		 * for (int i =1 ; i<=5; i++) { for (int j = 1; j <= 5 - i; j++) {
-		 * System.out.print(" ");
-		 * 
-		 * } for (int k =1; k <= i; k++) { System.out.print("*");
-		 * 
-		 * } System.out.println(""); }
-		 */
 		
-		// Pattern #4
-				for (int i =1 ; i<=5; i++) {
-					for (int j = 1; j <= 5 - i; j++) {
-						System.out.print(" ");
-					
-					} for (int k =1; k <= i; k++) {
-				System.out.print("*");
-				
-			} System.out.println("");
+		  for (int i =1 ; i<=5; i++) { for (int j = 1; j <= 5 - i; j++) {
+		  System.out.print(" ");
+		  
+		  } for (int k =1; k <= i; k++) { System.out.print("*");
+		  
+		  } System.out.println(""); }
+		 
+		
+		
 			}
 		
-}
 }
