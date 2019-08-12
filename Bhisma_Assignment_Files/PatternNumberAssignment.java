55329ccfe25f6ac94d95001879a7ee0114dbbda9
bhismakandel <bhisma.kandel@gmail.com>
2019-08-12 18:29:28 -0400

    Assignment uploaded

---
+++ Bhisma_Assignment_Files/PatternNumberAssignment.java
@@ -0,0 +1,26 @@
+package java_july;
+
+public class PatternNumberAssignment {
+
+	public static void main(String[] args) {
+		for (int i=1; i <= 5; i++)
+		{
+			for (int j=1; j <=i; j++) {
+				
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+					
+		for (int i = 4; i>=1; i--)
+		{
+			for (int j =1; j<=i ; j++) 
+			{
+				System.out.print(j);
+			}
+		
+				System.out.println();
+	}
+
+	}
+}
